de14dd31ad7fb9162238871bce6ccdf32a871084
RusoInfoso <ri.service@outlook.com>
2013-11-26 00:10:58 +0400

    6 in 7

--- CodedHomes.Web/Controllers/HomesController.cs
+++ CodedHomes.Web/Controllers/HomesController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CodedHomes.Data;
+using CodedHomes.Models;
 using CodedHomes.Web.ViewModels;
 
 namespace CodedHomes.Web.Controllers
@@ -11,7 +13,7 @@ namespace CodedHomes.Web.Controllers
     [Authorize]
     public class HomesController : Controller
     {
-        private ApplicationUnit _unit = new ApplicationUnit();
+        private readonly ApplicationUnit _unit = new ApplicationUnit();
 
         [AllowAnonymous]
         public ActionResult Index()
@@ -22,6 +24,80 @@ namespace CodedHomes.Web.Controllers
             vm.Homes = query.ToList();
 
             return View("Index", vm);
+        }
+
+        public ActionResult New()
+        {
+            HomeViewModel vm = new HomeViewModel();
+            vm.IsNew = true;
+
+
+            return View("Home", vm);
+        }
+
+        [ActionName("Edit")]
+        public ActionResult Get(int id)
+        {
+            HomeViewModel vm = new HomeViewModel { Home = _unit.Homes.GetById(id) };
+
+            if (vm.Home != null)
+            {
+                return View("Home", vm);
+            }
+
+            return View("NotFound");
+        }
+
+        [HttpPost]
+        public ActionResult UploadImage(HttpPostedFileBase image, int id)
+        {
+            JsonResult result;
+            Random rand = new Random();
+
+            string unique = rand.Next(1000000).ToString();
+            string ext = Path.GetExtension(image.FileName).ToLower();
+            string fileName = string.Format("{0}-{1}{2}", id, unique, ext);
+            string path = Path.Combine(HttpContext.Server.MapPath(Config.ImagesFolderPath), fileName);
+
+            if (ext == ".png" || ext == ".jpg")
+            {
+                Home home = _unit.Homes.GetById(id);
+
+                if (home != null)
+                {
+                    home.ImageName = fileName;
+                    _unit.Homes.Update(home);
+                    _unit.SaveChanges();
+
+                    image.SaveAs(path);
+                    result = this.Json(new
+                    {
+                        imageUrl = string.Format("{0}{1}",
+                        Config.ImagesUrlPrefix, fileName)
+                    });
+                }
+                else
+                {
+                    result = this.Json(new
+                    {
+                        status = "error",
+                        statusText =
+                            string.Format("There is no home with the Id of " +
+                                "'{0}' in the system.", id)
+                    });
+                }
+            }
+            else
+            {
+                result = this.Json(new
+                {
+                    status = "error",
+                    statusText = "Unsupported image type. Only .png or " +
+                        ".jpg files are acceptable."
+                });
+            }
+
+            return result;
         }
 
         protected override void Dispose(bool disposing)
---
+++ CodedHomes.Web/ViewModels/HomeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CodedHomes.Models;
+
+namespace CodedHomes.Web.ViewModels
+{
+    public class HomeViewModel : ViewModelBase
+    {
+        public Home Home { get; set; }
+        public bool IsNew { get; set; }
+
+        public string ImageUrlPrefix
+        {
+            get
+            {
+                return Config.ImagesUrlPrefix;
+            }
+        }
+
+        public HomeViewModel()
+        {
+            Home = new Home();
+        }
+    }
+}
